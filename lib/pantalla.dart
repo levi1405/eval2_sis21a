3dcdfa618cfd8721ab796ebed3a69d85b5a6f6f1
Douglas-prog23 <131540385+Douglas-prog23@users.noreply.github.com>
2023-09-27 08:15:03 -0600

    pantallas

---
+++ lib/pantalla.dart
@@ -0,0 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+ 
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+
+  void getEstudiantes() async{
+    CollectionReference collectionReference =
+    FirebaseFirestore.instance.collection("tbestudiantes");
+    QuerySnapshot mensaje = await collectionReference.get();
+    if (mensaje.docs.length !=0) {
+      for (var doc in mensaje.docs) {
+        print(doc.data());
+        //chatsx.add(doc.data());
+        
+      }
+    }
+  }
+
+  Future<List> getMensajes() async{
+    List chats = [];
+    CollectionReference collectionReference=
+    FirebaseFirestore.instance.collection("tbestudiantes");
+    QuerySnapshot mensajes = await collectionReference.get();
+    if(mensajes.docs.length !=0){
+      for(var doc in mensajes.docs){
+      print(doc.data());
+      chats.add(doc.data());
+      }
+    }
+    return chats;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+       
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder(
+        future: getMensajes(),
+        builder: ((context, snapshot){
+          if(snapshot.hasData){
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: ((context, index) {
+                return Text(snapshot.data?[index]["apellido"]+
+                snapshot.data?[index]["carnet"]
+                );
+              })
+            ); 
+          }else{
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        })
+      ),
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed:getEstudiantes,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getEstudiantes();
+  }
+
+ @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  
+
+}
